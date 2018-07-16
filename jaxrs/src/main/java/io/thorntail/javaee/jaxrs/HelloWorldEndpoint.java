20f481812621c8dede75b0addc960a1beb986487
albihasani94 <albihasani94@gmail.com>
2018-07-16 12:46:54 +0200

    Add logging to service and send logs to elasticsearch

--- jaxrs/src/main/java/io/thorntail/javaee/jaxrs/HelloWorldEndpoint.java
+++ jaxrs/src/main/java/io/thorntail/javaee/jaxrs/HelloWorldEndpoint.java
@@ -1,6 +1,8 @@
 package io.thorntail.javaee.jaxrs;
 
 
+import java.util.logging.Logger;
+
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.GET;
@@ -10,9 +12,12 @@ import javax.ws.rs.Produces;
 @Path("/hello")
 public class HelloWorldEndpoint {
 
+    private static final Logger LOGGER = Logger.getLogger("HelloWorldEndpoint");
+
     @GET
     @Produces("text/plain")
     public Response doGet() {
+        LOGGER.info("Calling hello service");
         return Response.ok("Hello from WildFly Swarm!").build();
     }
 
